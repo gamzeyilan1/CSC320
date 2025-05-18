ac975cc87fa77a7756eca056a33ff1c100816e41
Gamze Yılan <gamzeyilan@unknown2af85842e17d.home>
2025-05-18 23:43:23 +0300

    Initial commit

---
+++ src/Module1.java
@@ -0,0 +1,20 @@
+// Module 1 CTA
+// Option #1: List Individual's Name and Address Information
+// CSC320 - Programming I, Dr. Terrell Brandes
+public class Module1 {
+    public static void main(String[] args) {
+        // Step 1: Declare and initialize variables
+        String firstName = "Harry";
+        String lastName = "Potter";
+        String streetAddress = "4 Privet Drive";
+        String city = "Surrey";
+        String zipCode = "12345";
+
+        // Step 2: Print each value
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
